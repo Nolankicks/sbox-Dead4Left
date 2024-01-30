a709bfaff1082b90e246e3c17fa22d121af48426
Nolankicks <nolandimensions@gmail.com>
2024-01-29 20:14:46 -0500

    Update user authentication logic

---
+++ code/IHealthComponent.cs
@@ -0,0 +1,12 @@
+using System;
+using Sandbox;
+
+namespace Kicks
+{
+	public interface IHealthComponent
+	{
+		public float MaxHealth { get; }
+		public float Health { get; }
+		public void TakeDamage(float damage);
+	}
+}
