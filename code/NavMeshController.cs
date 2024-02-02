5b3ca76ea116152603cc7ab9bfd4c9cdfab865b9
Nolankicks <nolandimensions@gmail.com>
2024-02-02 16:47:40 -0500

    Update ZombiePrefab and finalmain.scene, and add NavMeshController

--- code/NavMeshController.cs
+++ code/NavMeshController.cs
@@ -1,19 +1,38 @@
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Kicks;
 using Sandbox;
 
 public sealed class NavMeshController : Component
 {
-	RealTimeSince timeSinceUpdate = 0;
 	
+	public NavMeshAgent agent;
+	private Vector3 _destination;
+	public CharacterController playerController;
+	RealTimeSince timeSinceUpdate = 0;
+	protected override void OnAwake()
+	{
+		agent = Components.Get<NavMeshAgent>();
+		playerController = Scene.GetAllComponents<CharacterController>().FirstOrDefault();
+		_destination = playerController.Transform.Position;
+	}
+
+
+
+
 	protected override void OnUpdate()
 	{
-		if (timeSinceUpdate > 0.5f)
+		_destination = playerController.Transform.Position;
+		
+		if ( timeSinceUpdate > 1 && agent != null)
 		{
-			timeSinceUpdate = 0f;
+			
+			timeSinceUpdate = 0;
 
-			foreach (var agent in Scene.GetAllComponents<NavMeshAgent>())
-			{
-				
-			}
+			agent.MoveTo( _destination );
+			
+		
+        
 		}
 	}
 }
