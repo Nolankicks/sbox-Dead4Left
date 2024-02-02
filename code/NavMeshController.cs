4aeddf2bb72ea30c915e6fe4fc1f7baa30aa08d9
Nolankicks <nolandimensions@gmail.com>
2024-02-02 16:47:25 -0500

    Remove unused code and optimize NavMeshController

--- code/NavMeshController.cs
+++ code/NavMeshController.cs
@@ -2,16 +2,17 @@ using Sandbox;
 
 public sealed class NavMeshController : Component
 {
-	[Property] public GameObject Target { get; set; }
 	RealTimeSince timeSinceUpdate = 0;
+	
 	protected override void OnUpdate()
 	{
-		if ( timeSinceUpdate >  1)
+		if (timeSinceUpdate > 0.5f)
 		{
-			timeSinceUpdate = 0;
-			foreach ( var agent in Scene.GetAllComponents<NavMeshAgent>() )
+			timeSinceUpdate = 0f;
+
+			foreach (var agent in Scene.GetAllComponents<NavMeshAgent>())
 			{
-				agent.MoveTo( Target.Transform.Position );
+				
 			}
 		}
 	}
