ee5770fbaec2b43df20e7278dfe833688f53df41
Nolankicks <nolandimensions@gmail.com>
2024-02-02 17:29:57 -0500

    Add animation helper to NavMeshController

--- code/NavMeshController.cs
+++ code/NavMeshController.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using Kicks;
 using Sandbox;
+using Sandbox.Citizen;
 
 public sealed class NavMeshController : Component
 {
@@ -11,30 +12,35 @@ public sealed class NavMeshController : Component
 	public CharacterController playerController;
 	RealTimeSince timeSinceUpdate = 0;
 	TimeSince timeSinceHit = 0;
+	[Property] public CitizenAnimationHelper animationHelper { get; set; }
 	protected override void OnAwake()
 	{
 		agent = Components.Get<NavMeshAgent>();
 		playerController = Scene.GetAllComponents<CharacterController>().FirstOrDefault();
 		_destination = playerController.Transform.Position;
+		
 	}
 	protected override void OnFixedUpdate()
 	{
+		
 		_destination = playerController.Transform.Position;
 		GameObject.Transform.Rotation = Rotation.LookAt(_destination - GameObject.Transform.Position);
 		if ( timeSinceUpdate > 1 && agent != null)
 		{
 			timeSinceUpdate = 0;
 			agent.MoveTo( _destination );
+			UpdateAnimations();
 		}
 		if (Vector3.DistanceBetween(_destination, GameObject.Transform.Position) < 100 && agent != null)
 		{
 			agent.Stop();
 			Log.Info("Stopped");
 			Trace();
+			
 		}
 		
 	}
-		void Trace()
+	void Trace()
 	{
 		var lookDir = GameObject.Transform.Rotation;
 		var tr = Scene.Trace.Ray(GameObject.Transform.Position, GameObject.Transform.Position + lookDir.Forward * 100).WithoutTags("bad").Run();
@@ -42,5 +48,11 @@ public sealed class NavMeshController : Component
 		{
 			Log.Info("Hit");
 		}		
-}
+	}
+	void UpdateAnimations()
+	{
+		animationHelper.WithWishVelocity( agent.WishVelocity );
+		animationHelper.WithVelocity( agent.Velocity );
+		animationHelper.MoveStyle = CitizenAnimationHelper.MoveStyles.Run;
+	}
 }
