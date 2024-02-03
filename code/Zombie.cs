237156ff998b925686a65f11ebd3809047e83ad6
Nolankicks <nolandimensions@gmail.com>
2024-02-02 19:39:31 -0500

    Refactor characterController in finalmain.scene and add Trace method to Zombie.cs

--- code/Zombie.cs
+++ code/Zombie.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Kicks;
 using Sandbox;
 using Sandbox.Citizen;
 
@@ -7,19 +9,36 @@ public sealed class Zombie : Component
 	[Property] public GameObject eyes { get; set; }
 	[Property] public CitizenAnimationHelper animationHelper { get; set; }
 	private NavMeshAgent agent;
+	private PlayerController playerController;
+	public TimeSince timeSinceHit = 0;
 	protected override void OnAwake()
 	{
 		agent = Components.Get<NavMeshAgent>();
+		playerController = Scene.GetAllComponents<PlayerController>().FirstOrDefault();
 	}
 	protected override void OnUpdate()
 	{
 		animationHelper.HoldType = CitizenAnimationHelper.HoldTypes.Swing;
 		animationHelper.MoveStyle = CitizenAnimationHelper.MoveStyles.Run;
 		UpdateAnimtions();
+		Trace();
 	}
 	void UpdateAnimtions()
 	{
 		animationHelper.WithWishVelocity(agent.WishVelocity);
 		animationHelper.WithVelocity(agent.Velocity);
 	}
+	void Trace()
+	{
+		var tr = Scene.Trace.Ray(body.Transform.Position, body.Transform.Position + body.Transform.Rotation.Forward * 160).Run();
+
+		if (tr.Hit && tr.GameObject.Tags.Has("player") && timeSinceHit > 1.0f)
+		{
+			playerController.TakeDamage(15);
+			animationHelper.Target.Set("b_attack", true);
+			timeSinceHit = 0;
+		}
+
+	}
+	
 }
