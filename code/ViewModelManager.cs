0afdc7ae497b36af1a1aa108b361dcba9a035f67
Nolankicks <nolandimensions@gmail.com>
2024-02-04 19:07:28 -0500

    Update inventory UI and weapon models

--- code/ViewModelManager.cs
+++ code/ViewModelManager.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Sandbox;
 
 public sealed class ViewModelManager : Component
@@ -7,20 +8,30 @@ public sealed class ViewModelManager : Component
 	[Property] Model mp5Model { get; set; }
 	[Property] Model fistsModel { get; set; }
 	[Property] Weapon weapon { get; set; }
-
-
+	private Vector3 cameraStartPos;
+	private CameraComponent viewModelCamera;
+	public Model blankModel;
+	protected override void OnAwake()
+	{
+		viewModelCamera = Components.GetInParent<CameraComponent>();
+		cameraStartPos = viewModelCamera.GameObject.Transform.LocalPosition;
+	}
 
 	protected override void OnUpdate()
 	{
 		if (weapon.ActiveSlot == 0)
 		{
 			weaponRender.Model = mp5Model;
+			armRender.Enabled = true;
+			viewModelCamera.Transform.LocalPosition = cameraStartPos;
 		}
 		else
 		{
 			weaponRender.Model = fistsModel;
-			
+			armRender.Enabled = false;
+			viewModelCamera.GameObject.Transform.LocalPosition = new Vector3(15, 0, -5);
 		}
+
 		
 	}
 }
--- code/Weapon.cs
+++ code/Weapon.cs
@@ -1,15 +1,25 @@
 using System;
 using System.Collections.Generic;
 using Sandbox;
+using Sandbox.UI;
 
 public sealed class Weapon : Component
 {
+	[Property] public Image WeaponImage {get; set;}
 	[Property] public List<string> Inventory {get; set;} = new List<string>()
 	{
 		"weapon_SMG"
 	};
+	
+	[Property] public List<string> InventoryImages {get; set;} = new List<string>()
+	{
+
+	};
+	
 	public int ActiveSlot = 0;
 	public int Slots => 9;
+	public int Img => 9;
+	
 	protected override void OnUpdate()
 	{
 		if (Input.MouseWheel.y != 0)
@@ -24,3 +34,4 @@ public sealed class Weapon : Component
 		Log.Info(ActiveSlot);
 	}
 }
+
