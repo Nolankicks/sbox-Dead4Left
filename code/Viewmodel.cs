1214895255cd746578d94b01db2ec30896a9a27e
Nolankicks <nolandimensions@gmail.com>
2024-02-06 19:08:46 -0500

    Update finalmain.scene and Viewmodel.cs

--- code/Viewmodel.cs
+++ code/Viewmodel.cs
@@ -2,15 +2,25 @@ using Sandbox;
 
 public sealed class Viewmodel : Component
 {
+	[Property] public GameObject eye { get; set; }
+	[Property] public GameObject arms { get; set; }
+	public Rotation rotation;
+	public Vector3 vector3;
+	protected override void OnAwake()
+	{
+		rotation = arms.Transform.LocalRotation;
+		vector3 = arms.Transform.LocalPosition;
+	}
 	protected override void OnUpdate()
 	{
 			if (Input.Pressed("Duck"))
 			{
-				GameObject.Transform.Position -= Vector3.Down * -32;
+				arms.Transform.Position -= Vector3.Down * -32;
 			}
 			if (Input.Released("Duck"))
 			{
-				GameObject.Transform.Position += Vector3.Up * 32;
+				arms.Transform.LocalPosition = vector3;
+				arms.Transform.LocalRotation = rotation;
 			}
 	}
 }
