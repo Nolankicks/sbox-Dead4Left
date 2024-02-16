7f3a1a115e8504a9b1bebfe496235abfc067641b
Nolankicks <nolandimensions@gmail.com>
2024-02-15 19:33:20 -0500

    Update dead4leftmenu.scene and JoinMenu.razor files

--- code/MainMenuZombiePoser.cs
+++ code/MainMenuZombiePoser.cs
@@ -1,11 +1,36 @@
+using System;
 using Sandbox;
 using Sandbox.Citizen;
 
 public sealed class MainMenuZombiePoser : Component
 {
 	[Property] public CitizenAnimationHelper AnimationHelper { get; set; }
+	float GetRandom() => Random.Shared.Float(0, 100);
 	protected override void OnStart()
 	{
 		AnimationHelper.HoldType = CitizenAnimationHelper.HoldTypes.Punch;
+	}
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+		
+	}
+	TimeUntil nextSecond;
+	protected override void OnFixedUpdate()
+	{
+		if (nextSecond)
+		{
+			var random = GetRandom();
+			Log.Info(random);
+			GetRandom();
+
+			if (random > 70)
+			{
+				AnimationHelper.Target.Set("b_attack", true);
+			}
+
+			nextSecond = 1;
+		}
 	}
 }
